3b6abcf23a87a4f89b53ec099a6fce8eb001cd5e
Uriel Klieger <uriel.klieger@cambridgeconsultants.com>
2023-12-20 00:34:46 -0500

    Add 4d support and a stata center model

--- viz.py
+++ viz.py
@@ -2,6 +2,7 @@ import argparse
 import pygame
 import sys
 import math
+from stl import mesh
 
 from scipy.spatial.transform import Rotation
 import numpy as np
@@ -26,7 +27,6 @@ class Viewport:
     def surface(self):
         return self.surface
 
-
     def get_width(self):
         return self.w
 
@@ -36,6 +36,97 @@ class Viewport:
     def blit(self, surf):
         surf.blit(self.surface, (self.x+self.border,self.y+self.border))
         pygame.draw.rect(surf, "white", (self.x,self.y,self.w,self.h), self.border)
+
+class obj4:
+    def __init__(self, points=[], edges=[]):
+        self.dim = 4
+        self.reset_points = points
+        self.reset_edges = edges
+        self.reset()
+
+    def reset(self):
+        self.points = self.reset_points
+        self.edges = self.reset_edges
+
+    @staticmethod
+    def from_3d(obj):
+        points = obj.points
+        edges = obj.edges
+
+        points = [point+[0] for point in points]
+
+        return obj4(points, edges)
+
+    def rotate(self, xy=0, xz=0, xw=0, yz=0, yw=0, zw=0, degrees=True):
+        if degrees:
+            xy = xy*(math.pi/180)
+            xz = xz*(math.pi/180)
+            xw = xw*(math.pi/180)
+            yz = yz*(math.pi/180)
+            yw = yw*(math.pi/180)
+            zw = zw*(math.pi/180)
+
+        rotation4d_xy= [
+            [math.cos(xy), -math.sin(xy), 0, 0],
+            [math.sin(xy), math.cos(xy), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]]
+        rotation4d_xz = [
+            [math.cos(xz), 0, -math.sin(xz), 0],
+            [0, 1, 0, 0],
+            [math.sin(xz), 0, math.cos(xz), 0],
+            [0, 0, 0, 1]]
+        rotation4d_xw = [
+            [math.cos(xw), 0, 0, -math.sin(xw)],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [math.sin(xw), 0, 0, math.cos(xw)]]
+        rotation4d_yz = [
+            [1, 0, 0, 0],
+            [0, math.cos(yz), -math.sin(yz), 0],
+            [0, math.sin(yz), math.cos(yz), 0],
+            [0, 0, 0, 1]]
+        rotation4d_yw = [
+            [1, 0, 0, 0],
+            [0, math.cos(yw), 0, -math.sin(yw)],
+            [0, 0, 1, 0],
+            [0, math.sin(yw), 0, math.cos(yw)]]
+        rotation4d_zw = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, math.cos(zw), -math.sin(zw)],
+            [0, 0, math.sin(zw), math.cos(zw)]]
+
+        #self.points = (self.points * (np.matrix(rotation_x) * np.matrix(rotation_y) * np.matrix(rotation_z))).tolist()
+        self.points = (self.points *
+            np.matrix(rotation4d_xy) * np.matrix(rotation4d_xz) * np.matrix(rotation4d_xw) *
+            np.matrix(rotation4d_yz) * np.matrix(rotation4d_yw) * np.matrix(rotation4d_zw)).tolist()
+
+    def translate(self, x=0, y=0, z=0, w=0):
+        self.points = [[point[0]+x, point[1]+y, point[2]+z, point[3]+w] for point in self.points]
+
+    def project(self, surface, x, y, dimx, dimy, dimz=None, dist=500):
+        #todo: throw error if dim[xyz] >= self.dim
+        if dimz is None:
+            for edge in self.edges:
+                pygame.draw.line(surface, edge[0],
+                    (x + self.points[edge[1]][dimx], y + self.points[edge[1]][dimy]),
+                    (x + self.points[edge[2]][dimx], y + self.points[edge[2]][dimy])
+                    )
+        else:
+            for edge in self.edges:
+                A = self.points[edge[1]]
+                B = self.points[edge[2]]
+                dA = dist/(dist+A[dimz])
+                dB = dist/(dist+B[dimz])
+                # pygame.draw.line(surface, edge[0],
+                #     (x + (A[dimx] * ((dist+A[dimz])/A[dimz])), y + (A[dimy] * ((dist+A[dimz])/A[dimz]))),
+                #     (x + (B[dimx] * ((dist+B[dimz])/B[dimz])), y + (B[dimy] * ((dist+B[dimz])/B[dimz])))
+                #     )
+                pygame.draw.line(surface, edge[0],
+                    (x + (A[dimx] * dA), y + (A[dimy] * dA)),
+                    (x + (B[dimx] * dB), y + (B[dimy] * dB))
+                    )
 
 class obj3:
     def __init__(self, points=[], edges=[]):
@@ -125,6 +216,63 @@ class Cube(obj3):
         ]
 
         super().__init__(points, edges)
+
+class Hypercube(obj4):
+    def __init__(self, d=1):
+        self.d = d
+        points = [
+            [-d, -d,  d,  d],
+            [ d, -d,  d,  d],
+            [ d,  d,  d,  d],
+            [-d,  d,  d,  d],
+            [-d, -d, -d,  d],
+            [ d, -d, -d,  d],
+            [ d,  d, -d,  d],
+            [-d,  d, -d,  d],
+            [-d, -d,  d, -d],
+            [ d, -d,  d, -d],
+            [ d,  d,  d, -d],
+            [-d,  d,  d, -d],
+            [-d, -d, -d, -d],
+            [ d, -d, -d, -d],
+            [ d,  d, -d, -d],
+            [-d,  d, -d, -d],
+        ]
+        edges = []
+        for m in range(4):
+            edges.append(["red", m, (m+1)%4])
+            edges.append(["red", m+4, (m+1)%4 + 4])
+            edges.append(["red", m, m+4])
+
+        for m in range(4):
+            edges.append(["white", 8+m, 8+(m+1)%4])
+            edges.append(["white", 8+m+4, 8+(m+1)%4 + 4])
+            edges.append(["white", 8+m, 8+m+4])
+
+        for m in range(8):
+            edges.append(["white", m, m+8])
+
+        super().__init__(points, edges)
+
+class Stata(obj3):
+    def __init__(self):
+        points, edges = self.load()
+        super().__init__(points, edges)
+
+    def load(self):
+        points = []
+        edges = []
+
+        obj = mesh.Mesh.from_file("stata-reduced.stl")
+        for pt in obj.points:
+            idx = [len(points), len(points)+1, len(points)+2]
+            points.append(pt[0:3].tolist())
+            points.append(pt[3:6].tolist())
+            points.append(pt[6:9].tolist())
+            edges.append(["white", idx[0], idx[1]])
+            edges.append(["white", idx[1], idx[2]])
+            edges.append(["white", idx[2], idx[0]])
+        return points, edges
 '''
 class Obj4D:
     def __init__(self, d=1):
@@ -166,11 +314,19 @@ def main(args):
     clock = pygame.time.Clock()
     pygame.font.init()
 
-    cube = Cube(100)
+    # stata = Stata()
+    # cube = Cube(100)
+    # rot = [0,0,0]
+    # trans = [0,0,0]
+    # ortho = False
 
-    rot = [0,0,0]
-    trans = [0,0,0]
+    # cube=obj4.from_3d(Stata())
+    # cube = obj4.from_3d(Cube(100))
+    cube = Hypercube(100)
+    rot = [0,0,0,0,0,0]
+    trans = [0,0,0,0]
     ortho = False
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
@@ -190,36 +346,69 @@ def main(args):
                     trans[2] -= 5
                 elif event.key == pygame.K_e and pygame.key.get_mods() & pygame.KMOD_SHIFT:
                     trans[2] += 5
-                elif event.key == pygame.K_w:
-                    rot[0] += 5
-                elif event.key == pygame.K_s:
-                    rot[0] -= 5
-                elif event.key == pygame.K_a:
-                    rot[1] -= 5
-                elif event.key == pygame.K_d:
-                    rot[1] += 5
+                elif event.key == pygame.K_r and pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    trans[3] -= 5
+                elif event.key == pygame.K_f and pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    trans[3] -= 5
                 elif event.key == pygame.K_q:
-                    rot[2] -= 5
+                    rot[0] += 5
+                elif event.key == pygame.K_a:
+                    rot[0] -= 5
+                elif event.key == pygame.K_w:
+                    rot[1] += 5
+                elif event.key == pygame.K_s:
+                    rot[1] -= 5
                 elif event.key == pygame.K_e:
                     rot[2] += 5
+                elif event.key == pygame.K_d:
+                    rot[2] -= 5
+                elif event.key == pygame.K_r:
+                    rot[3] += 5
+                elif event.key == pygame.K_f:
+                    rot[3] -= 5
+                elif event.key == pygame.K_t:
+                    rot[4] += 5
+                elif event.key == pygame.K_g:
+                    rot[4] -= 5
+                elif event.key == pygame.K_y:
+                    rot[5] += 5
+                elif event.key == pygame.K_h:
+                    rot[5] -= 5
                 elif event.key == pygame.K_o:
                     ortho = not ortho
+                elif event.key == pygame.K_p:
+                    trans = [0 for t in trans]
+                    rot = [0 for r in rot]
+
+        rot = [angle % 360 for angle in rot]
 
         # Start with a black screen
         screen.fill("black")
 
         # Set up a window for drawing
-        W = Viewport(width-30, height-30, 15, 15)
+        W = Viewport((width-45)/2, height-30, 15, 15)
+        X = Viewport((width-45)/2, height-30, 30+((width-45)/2), 15)
 
         cube.reset()
         cube.rotate(*rot)
         cube.translate(*trans)
         if ortho:
             cube.project(W.surface, W.get_width()/2, W.get_height()/2, 0, 1)
+            cube.project(X.surface, W.get_width()/2, W.get_height()/2, 0, 3)
         else:
             cube.project(W.surface, W.get_width()/2, W.get_height()/2, 0, 1, 2, 500)
+            cube.project(X.surface, W.get_width()/2, W.get_height()/2, 1, 2, 3, 500)
+
+        # stata.reset()
+        # stata.rotate(*rot)
+        # stata.translate(*trans)
+        # if ortho:
+        #  stata.project(W.surface, W.get_width()/2, W.get_height()/2, 0, 1)
+        # else:
+        #  stata.project(W.surface, W.get_width()/2, W.get_height()/2, 0, 1, 2, 500)
 
         W.blit(screen)
+        X.blit(screen)
 
         # Update the status bar
         status(screen, f"R{rot} T{trans} {'ortho' if ortho else 'perspective'}")
